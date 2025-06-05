06d425a64a484c13db8ff24fe9a8b1c484bf422e
nachosag <ignacioborlenghi@gmail.com>
2025-06-05 23:06:14 +0000

    Add main application file for Expense Tracker API

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from src.database.core import lifespan
+
+app = FastAPI(
+    title="Expense Tracker RESTful API",
+    lifespan=lifespan,
+    root_path="/api/v1",
+    version="0.1.0",
+)
