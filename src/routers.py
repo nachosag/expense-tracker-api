7471f17eca6a4ee2706afd1ddc0699f66f431e22
nachosag <ignacioborlenghi@gmail.com>
2025-06-06 06:20:16 +0000

    Add authentication router to the FastAPI application

--- src/routers.py
+++ src/routers.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from .modules.users.user_controller import users_router
+from .modules.auth.auth_controller import auth_router
+
 # from .modules.expenses.expense_controller
-# from .modules.auth.auth_controller
 
 
 def add_routers(app: FastAPI):
     app.include_router(users_router, tags=["USERS"])
+    app.include_router(auth_router, tags=["AUTH"], prefix="/auth")
