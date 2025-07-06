466250717aac63f6564dd4637d4e4d35ede38079
nachosag <ignacioborlenghi@gmail.com>
2025-07-06 19:57:04 -0300

    Refactor auth controller

--- src/modules/auth/auth_controller.py
+++ src/modules/auth/auth_controller.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter, status, Depends
 from fastapi.security import OAuth2PasswordRequestForm
-from datetime import timedelta
 from ...database.core import SessionDependency
-from . import auth_service, auth_schemas, config
+from . import auth_service, auth_schemas
 
 auth_router = APIRouter()
 
@@ -14,13 +13,7 @@ def login(
     session: SessionDependency,
     form_data: OAuth2PasswordRequestForm = Depends(),
 ):
-    user = auth_service.authenticate_user(
-        form_data.username, form_data.password, session
-    )
-    token = auth_service.create_access_token(
-        user.email, user.id, timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
-    )
-    return auth_schemas.Token(access_token=token, token_type="bearer")
+    return auth_service.login(form_data.username, form_data.password, session)
 
 
 @auth_router.post(
