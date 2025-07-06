2a0b41d72c4968bcf44251ca1aab0e2cb18a41f5
nachosag <ignacioborlenghi@gmail.com>
2025-07-06 20:18:51 -0300

    Refactor expense module

--- src/modules/expenses/expense_controller.py
+++ src/modules/expenses/expense_controller.py
@@ -1,5 +1,6 @@
 from datetime import date
-from . import expense_schema, expense_service
+from . import expense_schema
+from .expense_service import ExpenseService
 from ...database.core import SessionDependency
 from ..auth.auth_service import TokenDependency
 from fastapi import APIRouter, status
@@ -17,8 +18,7 @@ def create_expense(
     token: TokenDependency,
     request: expense_schema.CreateExpenseRequest,
 ):
-    return expense_service.create_expense(session, token, request)
-
+    return ExpenseService.create(session, token, request)
 
 @expenses_router.get(
     path="/",
@@ -31,17 +31,24 @@ def list_expenses(
     from_date: date | None = None,
     to_date: date | None = None,
 ):
-    return expense_service.list_expenses(from_date, to_date, session, token)
-
+    return ExpenseService.list(
+        session,
+        token,
+        from_date,
+        to_date,
+    )
 
 @expenses_router.get(
     path="/{expense_id}",
     response_model=expense_schema.ExpenseResponse,
     status_code=status.HTTP_200_OK,
 )
-def get_expense(expense_id: int, session: SessionDependency, token: TokenDependency):
-    return expense_service.get_expense(expense_id, session, token)
-
+def get_expense(
+    session: SessionDependency,
+    token: TokenDependency,
+    expense_id: int,
+):
+    return ExpenseService.get(session, token, expense_id)
 
 @expenses_router.patch(
     path="/{expense_id}",
@@ -49,17 +56,16 @@ def get_expense(expense_id: int, session: SessionDependency, token: TokenDepende
     status_code=status.HTTP_200_OK,
 )
 def update_expense(
-    expense_id: int,
-    request: expense_schema.UpdateExpenseRequest,
     session: SessionDependency,
     token: TokenDependency,
+    expense_id: int,
+    request: expense_schema.UpdateExpenseRequest,
 ):
-    return expense_service.update_expense(expense_id, request, session, token)
-
+    return ExpenseService.update(session, token, expense_id, request)
 
 @expenses_router.delete(
     path="/{expense_id}",
     status_code=status.HTTP_204_NO_CONTENT,
 )
 def delete_expense(expense_id: int, session: SessionDependency, token: TokenDependency):
-    return expense_service.delete_expense(expense_id, session, token)
+    return ExpenseService.delete(session, token, expense_id)
--- src/modules/expenses/expense_service.py
+++ src/modules/expenses/expense_service.py
@@ -9,110 +9,129 @@ from src.modules.expenses import expense_schema
 from ...database import models
 
 
-def validate_category_id(session: SessionDependency, category_id: int):
-    category = session.exec(
-        select(models.Category).where(models.Category.id == category_id)
-    ).first()
-    if not category:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Category_id not found"
-        )
+class ExpenseService:
 
+    @staticmethod
+    def validate_category_id(session: SessionDependency, category_id: int):
+        category = session.exec(
+            select(models.Category).where(models.Category.id == category_id)
+        ).first()
+        if not category:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Category_id not found"
+            )
 
-def create_expense(
-    session: SessionDependency,
-    token: TokenDependency,
-    request: expense_schema.CreateExpenseRequest,
-):
-    validate_category_id(session, request.category_id)
-    token_data = auth_service.get_current_user(token=token)
-    if not token_data.user_id:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated"
+    @staticmethod
+    def create(
+        session: SessionDependency,
+        token: TokenDependency,
+        request: expense_schema.CreateExpenseRequest,
+    ):
+        ExpenseService.validate_category_id(session, request.category_id)
+        token_data = auth_service.get_current_user(token=token)
+        if not token_data.user_id:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated"
+            )
+        try:
+            expense = models.Expense(**request.model_dump(), user_id=token_data.user_id)
+            session.add(expense)
+            session.commit()
+            session.refresh(expense)
+            logging.info("Expense created correctly")
+            return expense
+        except Exception as e:
+            logging.error("An unexpected error has occurred", e)
+            session.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail="Unexpected error"
+            )
+
+    @staticmethod
+    def get(
+        session: SessionDependency,
+        token: TokenDependency,
+        expense_id: int,
+    ):
+        token_data = auth_service.get_current_user(token=token)
+        stmt = select(models.Expense).where(
+            models.Expense.id == expense_id, models.Expense.user_id == token_data.user_id
         )
-    try:
-        expense = models.Expense(**request.model_dump(), user_id=token_data.user_id)
-        session.add(expense)
-        session.commit()
-        session.refresh(expense)
-        logging.info("Expense created correctly")
+        expense = session.exec(stmt).first()
+        if not expense:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Resource not found"
+            )
         return expense
-    except Exception as e:
-        logging.error("An unexpected error has occurred", e)
-        session.rollback()
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT, detail="Unexpected error"
+
+    @staticmethod
+    def list(
+        session: SessionDependency,
+        token: TokenDependency,
+        from_date: date | None,
+        to_date: date | None,
+    ):
+        token_data = auth_service.get_current_user(token=token)
+        stmt = select(models.Expense).where(models.Expense.user_id == token_data.user_id)
+
+        if from_date:
+            stmt = stmt.where(models.Expense.spent_at >= from_date)
+        if to_date:
+            stmt = stmt.where(models.Expense.spent_at <= to_date)
+
+        return session.exec(stmt).all()
+
+    @staticmethod
+    def update(
+        session: SessionDependency,
+        token: TokenDependency,
+        expense_id: int,
+        request: expense_schema.UpdateExpenseRequest,
+    ):
+        expense = ExpenseService.get(
+            session,
+            token,
+            expense_id,
         )
 
+        for attr, val in request.model_dump(exclude_unset=True, exclude_none=True).items():
+            if hasattr(expense, attr) and val != getattr(expense, attr):
+                setattr(expense, attr, val)
 
-def get_expense(expense_id: int, session: SessionDependency, token: TokenDependency):
-    token_data = auth_service.get_current_user(token=token)
-    stmt = select(models.Expense).where(
-        models.Expense.id == expense_id, models.Expense.user_id == token_data.user_id
-    )
-    expense = session.exec(stmt).first()
-    if not expense:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Resource not found"
+        expense.updated_at = datetime.now()
+        try:
+            session.add(expense)
+            session.commit()
+            session.refresh(expense)
+            logging.info("Expense updated correctly")
+            return expense
+        except Exception as e:
+            session.rollback()
+            logging.error("An unexpected error has occurred", e)
+            raise HTTPException(
+                status_code=status.HTTP_304_NOT_MODIFIED,
+                detail="An unexpected error occurred",
+            )
+
+    @staticmethod
+    def delete(
+        session: SessionDependency,
+        token: TokenDependency,
+        expense_id: int,
+    ):
+        expense = ExpenseService.get(
+            session,
+            token,
+            expense_id,
         )
-    return expense
-
-
-def list_expenses(
-    from_date: date | None,
-    to_date: date | None,
-    session: SessionDependency,
-    token: TokenDependency,
-):
-    token_data = auth_service.get_current_user(token=token)
-    stmt = select(models.Expense).where(models.Expense.user_id == token_data.user_id)
-
-    if from_date:
-        stmt = stmt.where(models.Expense.spent_at >= from_date)
-    if to_date:
-        stmt = stmt.where(models.Expense.spent_at <= to_date)
-
-    return session.exec(stmt).all()
-
-
-def update_expense(
-    expense_id: int,
-    request: expense_schema.UpdateExpenseRequest,
-    session: SessionDependency,
-    token: TokenDependency,
-):
-    expense = get_expense(expense_id, session, token)
-
-    for attr, val in request.model_dump(exclude_unset=True, exclude_none=True).items():
-        if hasattr(expense, attr) and val != getattr(expense, attr):
-            setattr(expense, attr, val)
-
-    expense.updated_at = datetime.now()
-    try:
-        session.add(expense)
-        session.commit()
-        session.refresh(expense)
-        logging.info("Expense updated correctly")
-        return expense
-    except Exception as e:
-        session.rollback()
-        logging.error("An unexpected error has occurred", e)
-        raise HTTPException(
-            status_code=status.HTTP_304_NOT_MODIFIED,
-            detail="An unexpected error occurred",
-        )
-
-
-def delete_expense(expense_id: int, session: SessionDependency, token: TokenDependency):
-    expense = get_expense(expense_id, session, token)
-    try:
-        session.delete(expense)
-        session.commit()
-        logging.info("Expense deleted correctly")
-    except Exception as e:
-        session.rollback()
-        logging.error("An unexpected error has occurred", e)
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="An unexpected error occurred",
-        )
+        try:
+            session.delete(expense)
+            session.commit()
+            logging.info("Expense deleted correctly")
+        except Exception as e:
+            session.rollback()
+            logging.error("An unexpected error has occurred", e)
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="An unexpected error occurred",
+            )
